995de6b3292f726c3f2a141990076e22b5437c97
Blacktonystark <gregorie.thomas@gmail.com>
2021-10-01 10:37:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// declarations
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const app = express();
+
+// mongodb connect
+mongoose.connect("mongodb://localhost:27017/node-auth-yt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app
