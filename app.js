641323b5cef504512ee2881606092dd66eca688a
Blacktonystark <gregorie.thomas@gmail.com>
2021-10-01 13:22:56 -0400

    commit

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ mongoose.connect("mongodb://localhost:27017/loginsystem", {
     useUnifiedTopology: true,
 });
 
+//Mongo User Schema
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
 // Middleware
 app.engine('hbs', hbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
@@ -29,3 +43,37 @@ app.use(express.json());
 //passport.json
 app.use(passport.initialize());
 app.set(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new localStrategy(function(username, password, done) {
+    User,
+    findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user)
+            return done(null, false, { message: 'incorrect username.' });
+
+        bcrypt.compare(password, user.password, function(err, res) {
+            if (err) return done(err);
+            if (res === false) return done(null, false, { message: 'Incorrect Password.' });
+            return done(null, user);
+
+        });
+    });
+}));
+
+app.get('/', (req, res)() => {
+    res.render("index", { title: "Home" });
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
